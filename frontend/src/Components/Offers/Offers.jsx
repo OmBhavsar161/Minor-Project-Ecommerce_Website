94d6f4f6c015fb5fef1ea4adc4e88b73ea1c4801
OmBhavsar161 <ombhavsar2004@gmail.com>
2024-08-25 23:52:01 +0530

    coding...

--- frontend/src/Components/Offers/Offers.jsx
+++ frontend/src/Components/Offers/Offers.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const Offers = () => {
   return (
     <div className='flex w-[90%] h-[80vh] m-auto py-0 px-[140px] mb-[150px] bg-gradient-to-r from-purple-300 via-cyan-300 to-pink-300 rounded-lg'>
-      lin<div className="flex flex-1 flex-col justify-center ">
+      <div className="flex flex-1 flex-col justify-center ">
         <h1 className='text-7xl text-gray-900 font-semibold pb-8'>Exclusive</h1>
         <h1 className='text-7xl text-gray-900 font-semibold'>Offers For You</h1>
         <p className='text-gray-900 font-semibold text-[22px] mt-4'>ONLY ON BEST SELLERS PRODUCT</p>
