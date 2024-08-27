ec3242aed35cbd8175a0a689472a163bac26d2bf
OmBhavsar161 <ombhavsar2004@gmail.com>
2024-08-27 21:34:10 +0530

    Changes

--- backend/index.js
+++ backend/index.js
@@ -10,39 +10,41 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-// Database connection with Mongodb 
-mongoose.connect("mongodb+srv://ombhavsar2004:SrZW4BYUgSoByXmJ@cluster0.u9d68.mongodb.net/e-commerce");
+// Database connection with Mongodb
+mongoose.connect(
+  "mongodb+srv://ombhavsar2004:SrZW4BYUgSoByXmJ@cluster0.u9d68.mongodb.net/e-commerce"
+);
 
 // API Creaction
 app.get("/", (req, res) => {
-    res.send("Express App is Running")
-})
+  res.send("Express App is Running");
+});
 
 // Image Storage Engine
 const storage = multer.diskStorage({
-    destination: './Upload/images',
-    filename: (req, file, cb) => {
-        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
-})
-const upload = multer({storage: storage})
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+const upload = multer({ storage: storage });
 
 // Creating Upload Endpoint for images
-app.use('/images', express.static('Upload/images'))
-app.post("/Upload", upload.single("product"), (req, res) => {
-    res.json({
-        success: 1,
-        image_url: `http://localhost:${port}/images/${req.file.filename}`
-    })
-}) 
+app.use("/images", express.static("upload/images"));
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+  });
+});
 
-app.listen(port, (error) =>{
-    if(!error)
-        {
-        console.log("Server Running on Port " + port);
-    }
-    else
-    {
-        console.log("Error : " + error);
-    }
+app.listen(port, (error) => {
+  if (!error) {
+    console.log("Server Running on Port " + port);
+  } else {
+    console.log("Error : " + error);
+  }
 });
