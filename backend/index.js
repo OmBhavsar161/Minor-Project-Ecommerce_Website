3872260324aa77832ede63bdd83518133dcf69d0
OmBhavsar161 <ombhavsar2004@gmail.com>
2024-08-28 14:56:34 +0530

    Adding Post method

--- backend/index.js
+++ backend/index.js
@@ -72,15 +72,39 @@ const Product = mongoose.model("Product", {
     type: Date,
     default: Date.now,
   },
-  available:{
-    typr: Boolean,
-    default: true
-  }
+  available: {
+    type: Boolean,
+    default: true,
+  },
 });
 
-app.post('/addproduct', async (req, res) => {
+app.post("/addproduct", async (req, res) => {
+  let products = await Product.find({});
+  let id;
+  if (products.length > 0) {
+    let last_product_array = products.slice(-1);
+    let last_product = last_product_array[0];
+    id = last_product.id + 1;
+  } else {
+    id = 1;
+  }
 
-})
+  const product = new Product({
+    id: id,
+    name: req.body.name,
+    image: req.body.image,
+    category: req.body.category,
+    new_price: req.body.new_price,
+    old_price: req.body.old_price,
+  });
+  console.log(product);
+  await product.save();
+  console.log("saved");
+  res.json({
+    success: true,
+    name: req.body.name,
+  });
+});
 
 app.listen(port, (error) => {
   if (!error) {
